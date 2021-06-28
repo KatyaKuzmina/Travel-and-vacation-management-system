f0fbcbaff0be161762cf6ca9168448a637faa43e
KatyaKuzmina <84267401+KatyaKuzmina@users.noreply.github.com>
2021-06-28 17:54:58 +0300

    Add files via upload

---
+++ app/Http/Controllers/FavouriteController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use DB;
+use Illuminate\Http\Request;
+use App\Models\Accommodation;
+use App\Models\AccommodationFeedback;
+use App\Models\VacationPackages;
+use App\Models\User;
+use App\Models\Favourites;
+
+class FavouriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(request $request)
+    {
+        $favaccommodations = Accommodation::all();
+        $favvacations = VacationPackages::all();
+        $users = Auth::user();
+        return view('favourites',compact('favaccommodations', 'favvacations',  'users'));
+        
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function likes($id)
+    {
+       
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+       
+    }
+}
